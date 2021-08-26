f2445ef97d5c311dfcb0032793c475afb473874c
wxyzcctv <2846629226@qq.com>
2021-08-26 23:13:28 +0800

    实现返回success的登录接口

--- routers/api/users.js
+++ routers/api/users.js
@@ -28,10 +28,9 @@ router.post('/register', (req, res) => {
             } else {
                 // 如果数据库中没有发现所填写的邮箱就创建一个请求体
                 // 这个请求体中包括如下内容
-                // --------------------
+
                 // 通过gravatar处理邮箱的头像，不传的话默认是mm，这个mm会是一个头像的背景图
                 const avatar = gravatar.url(req.body.email, { defaultIcon: 'mm', size: 200 });
-                // --------------------
 
                 const newUser = new User({
                     name: req.body.name,
@@ -39,11 +38,11 @@ router.post('/register', (req, res) => {
                     avatar,
                     password: req.body.password,
                 })
-
+                // 进行密码加密，使用方法主要看npm
                 bcrypt.genSalt(10, function (err, salt) {
                     bcrypt.hash(newUser.password, salt, (err, hash) => {
                         if (err) throw err;
-
+                        // 这里的hash就是加密之后的结果
                         newUser.password = hash;
 
                         newUser.save()
@@ -52,6 +51,32 @@ router.post('/register', (req, res) => {
                     });
                 });
             }
+        })
+})
+
+// $route POST /api/users/login
+// @desc 返回token jwt passport
+// @access public
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    // 查询数据库
+    User.findOne({ email })
+        .then((user) => {
+            // 如果用户不存在就返回404状态码，并给出提示信息：邮箱不存在
+            if (!user) {
+                return res.status(404).json({ email: '邮箱不存在' })
+            }
+            // 密码匹配
+            bcrypt.compare(password, user.password)
+                .then((isMath) => {
+                    if (isMath) {
+                        res.json({ msg: 'success' })
+                    } else {
+                        return res.status(400).json({ password: '密码错误' })
+                    }
+                })
         })
 })
 
