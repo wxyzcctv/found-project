bde8f1e840f3d6b3b5c5568b8f5b797b0d4db2c5
wxyzcctv <2846629226@qq.com>
2021-08-30 22:43:16 +0800

    增加资金新增接口

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://admin:147258369@result-api-prod.yinl2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://testUser001:test1234@cluster0.omhri.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     secretOrKey: 'secret'
 }
--- routers/api/profiles.js
+++ routers/api/profiles.js
@@ -12,4 +12,21 @@ router.get('/test', (req, res) => {
     res.json({ msg: "profile works" })
 })
 
+// $route GET /api/profiles/add
+// @desc 返回请求的json数据
+// @access private
+router.post('/add', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const profileFields = {};
+    if (req.body.type) profileFields.type = req.body.type;
+    if (req.body.describe) profileFields.describe = req.body.describe;
+    if (req.body.income) profileFields.income = req.body.income;
+    if (req.body.expend) profileFields.expend = req.body.expend;
+    if (req.body.cash) profileFields.cash = req.body.cash;
+    if (req.body.remark) profileFields.remark = req.body.remark;
+
+    new Profile(profileFields).save().then(profile => {
+        res.json(profile)
+    })
+})
+
 module.exports = router;
