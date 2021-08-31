c112a20582dcb657f8950e3e8f348e38bbc4d237
wxyzcctv <2846629226@qq.com>
2021-08-31 09:05:13 +0800

    新增编辑删除接口

--- routers/api/profiles.js
+++ routers/api/profiles.js
@@ -53,4 +53,40 @@ router.get('/:id', passport.authenticate('jwt', { session: false }), (req, res) 
     }).catch(err => res.status(404).json(err))
 })
 
+// $route POST /api/profiles/edit
+// @desc 编辑信息接口
+// @access private
+router.post('/edit/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const profileFields = {};
+    if (req.body.type) profileFields.type = req.body.type;
+    if (req.body.describe) profileFields.describe = req.body.describe;
+    if (req.body.income) profileFields.income = req.body.income;
+    if (req.body.expend) profileFields.expend = req.body.expend;
+    if (req.body.cash) profileFields.cash = req.body.cash;
+    if (req.body.remark) profileFields.remark = req.body.remark;
+
+
+    // Profile.findOneAndUpdate的参数是：
+    // 第一个参数表示需要更新的数据的id值，
+    // 第二个参数表示需要更新的内容，
+    // 第三个参数表示为是否为新的内容
+    Profile.findOneAndUpdate(
+        { _id: req.params.id },
+        { $set: profileFields },
+        { new: true },
+    ).then(profile => res.json(profile))
+})
+
+// $route DELETE /api/profiles/delete/:id
+// @desc 删除信息接口
+// @access private
+router.delete(
+    '/delete/:id',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        Profile.findOneAndRemove({ _id: req.params.id })
+            .then(profile => res.json(profile))
+            .catch(err => res.status(404).json('删除失败'))
+    })
+
 module.exports = router;
